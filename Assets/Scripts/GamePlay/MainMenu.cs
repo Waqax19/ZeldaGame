a1a7dac8f9cd053c0545d145f07d05f0f6bc9c39
Waqax19 <wa9p4eabrm@gmail.com>
2024-09-18 15:48:30 +0300

    NewResources

--- Assets/Scripts/GamePlay/MainMenu.cs
+++ Assets/Scripts/GamePlay/MainMenu.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void changeScene(string sceneName)
     {
-        
+        SceneManager.LoadScene(sceneName);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
